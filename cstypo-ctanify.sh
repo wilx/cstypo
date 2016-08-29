26c8032a189f4b997c5d31a9487f31ee6ab8fb97
Vaclav Haisman <vhaisman@gmail.com>
2016-08-29 23:59:32 +0200

    Split enable/disable macros into own file.
    Split enable/disable macros definitions into own file and use `\def`
    instead of `\newcommand` to potentially allow using this package in
    plain (Lua)TeX.

--- cstypo-ctanify.sh
+++ cstypo-ctanify.sh
@@ -7,11 +7,13 @@ latexmk -gg -lualatex cstypotest.tex
 latexmk -gg -lualatex cstypo.tex
 
 DOCDIR=doc/lualatex/cstypo
-TEXDIR=tex/lualatex/cstypo
+LATEXDIR=tex/lualatex/cstypo
+TEXDIR=tex/luatex/cstypo
 
 ctanify --pkgname=cstypo \
         cstypo.lua=$TEXDIR \
-        cstypo.sty=$TEXDIR \
+        cstypo.sty=$LATEXDIR \
+        cstypo-tex.tex=$TEXDIR \
         README.md=$DOCDIR \
         cstypotest.tex=$DOCDIR \
         cstypotest.pdf=$DOCDIR \
