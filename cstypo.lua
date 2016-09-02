1bbfca9a7e61e0e971485604139eb894e70cf200
Vaclav Haisman <vhaisman@gmail.com>
2016-09-02 15:59:21 +0200

    Penalize breaks after § symbol.

--- cstypo.lua
+++ cstypo.lua
@@ -135,3 +135,42 @@ function cstypo_percents_disable()
   print('cstypo: Disabling percents hook.')
   luatexbase.remove_from_callback("pre_linebreak_filter", "cstypopercents")
 end
+
+
+local function prevent_paragraph (head)
+  while head do
+    -- glyph
+    if (head.id == GLYPH
+        -- and in Czech or unspecified (???)
+          and (head.lang == nil
+               or head.lang == CZECH_ID)) then
+      -- only if we are at paragraph symbol
+      if unicode.utf8.match(unicode.utf8.char(head.char), "[§]") then
+        -- and right of it is a space
+        if (head.next.id == GLUE
+              and (head.next.next.id == GLYPH
+                     and unicode.utf8.match(unicode.utf8.char(head.next.next.char),
+                                            "[0-9]"))) then
+          -- then avoid line break between the paragraph and the number
+          -- following it
+          local p = node.new("penalty")
+          p.penalty = 10000
+          node.insert_after(head, head, p)
+        end
+      end
+    end
+    head = head.next
+  end
+  return true
+end
+
+function cstypo_paragraph_enable()
+  print('cstypo: Enabling paragraph hook.')
+  luatexbase.add_to_callback("pre_linebreak_filter", prevent_paragraph,
+                             "cstypoparagraph")
+end
+
+function cstypo_paragraph_disable()
+  print('cstypo: Disabling paragraph hook.')
+  luatexbase.remove_from_callback("pre_linebreak_filter", "cstypoparagraph")
+end
